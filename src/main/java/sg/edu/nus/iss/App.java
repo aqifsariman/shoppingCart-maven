74c675b2a49292f2ab8b25c052c07288c5d06b2f
aqifsariman <aqifsariman@gmail.com>
2023-01-16 15:14:39 +0800

    Second commit.

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -2,6 +2,7 @@ package sg.edu.nus.iss;
 
 import java.io.Console;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,9 +11,11 @@ public final class App {
     private App() {
     }
 
-    public static void main(String[] args) {
-        String dirPath = "\\data";
+    public static void main(String[] args) throws IOException {
+        String strValue = "";
         String filename = "";
+        String dirPath = "c:\\Users\\aqifs\\ibf\\day3_workshop\\data";
+
         File newDirectory = new File(dirPath);
 
         if (!newDirectory.exists()) {
@@ -27,6 +30,7 @@ public final class App {
         Console cons = System.console();
 
         String input = "";
+
         while (!input.equals("quit")) {
             input = cons.readLine("What do you want to perform? (Type 'quit' if you want to exit the program.)\n");
 
@@ -39,13 +43,7 @@ public final class App {
                     System.out.println("'quit' to exit this program.");
                     break;
                 case "list":
-                    if (cartItems.size() > 0) {
-                        for (String item : cartItems) {
-                            System.out.printf("%d. %s\n", cartItems.indexOf(item) + 1, item);
-                        }
-                    } else {
-                        System.out.println("Cart is empty.");
-                    }
+                    listCart(cartItems);
                     break;
                 case "users":
                     break;
@@ -55,16 +53,58 @@ public final class App {
             if (input.startsWith("add")) {
                 input = input.replace(",", " ");
                 Scanner scan = new Scanner(input.substring(4));
-                String strValue = "";
+                strValue = "";
 
                 while (scan.hasNext()) {
                     strValue = scan.next();
-                    cartItems.add(strValue);
-                    System.out.printf("%s added to cart.\n", strValue);
+                    if (!cartItems.contains(strValue)) {
+                        cartItems.add(strValue);
+                        System.out.printf("%s added to cart.\n", strValue);
+                    }
+                }
+
+            }
+            if (input.startsWith("delete")) {
+                deleteCartItem(cartItems, input);
+            }
+
+            if (input.startsWith("login")) {
+                input = input.replace(",", " ");
+
+                String strLogin = "";
+                Scanner scan = new Scanner(input.substring(6));
+
+                while (scan.hasNext()) {
+                    filename = scan.next();
+                }
+
+                File loginFile = new File(dirPath + File.separator + filename + ".txt");
+                boolean isCreated = loginFile.createNewFile();
+
+                if (isCreated) {
+                    System.out.printf("New file (%s) successfully created.", loginFile.getCanonicalFile());
+                } else {
+                    System.out.println("File already created.");
                 }
 
             }
         }
+    }
 
+    public static void listCart(List<String> cartItems) {
+        if (cartItems.size() > 0) {
+            for (String item : cartItems) {
+                System.out.printf("%d. %s\n", cartItems.indexOf(item) + 1, item);
+            }
+        } else {
+            System.out.println("Cart is empty.");
+        }
+    }
+
+    public static void deleteCartItem(List<String> cartItems, String input) {
+        Scanner scan = new Scanner(input);
+        String strValue = input.substring(7);
+        int indexValue = Integer.parseInt(strValue) - 1;
+        cartItems.remove(indexValue);
     }
 }
